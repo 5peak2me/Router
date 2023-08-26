d9e41c2c40d154ce0c68c55bf4ce3cb06c9b2b97
J!nl!n <461260911@qq.com>
2023-08-26 14:10:06 +0800

    Update plugin build configuration

--- router-plugin/build.gradle.kts
+++ router-plugin/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("java-gradle-plugin")
 //    id("org.jetbrains.kotlin.jvm")
 //    id("org.jetbrains.kotlin.jvm") version "1.7.20"
-    id("com.jfrog.artifactory") version "4.29.2"
+//    id("com.jfrog.artifactory") version "4.29.2"
     `kotlin-dsl`
 }
 
@@ -31,22 +31,25 @@ gradlePlugin {
 }
 
 dependencies {
-//    implementation("com.chenenyu.router:router:1.7.6")
-
-    implementation("com.joom.grip:grip:0.9.1")
-//    implementation("org.json:json:20220924")
-
     implementation(gradleApi())
 
-//    implementation("com.android.tools.build:gradle-api:7.4.2")
-    // https://developer.android.com/studio/releases/gradle-plugin-api-updates?hl=zh-cn#support_for_transformations_based_on_whole_program_analysis
-    implementation("com.android.tools.build:gradle:7.4.2")
-//    compileOnly("com.android.tools:common:30.1.2")
+    implementation("com.joom.grip:grip:0.9.1")
 
-    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+//    compileOnly("com.android.tools.build:gradle:8.1.1")
+    compileOnly("com.android.tools.build:gradle-api:8.1.1")
+    compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
 
-    // https://search.maven.org/artifact/org.ow2.asm/asm-commons/9.4/jar
-    implementation("org.ow2.asm:asm-commons:9.4") // 包含 asm 以及 asm-tree
-    implementation("org.ow2.asm:asm-util:9.4") // 包含 asm 以及 asm-tree、asm-analysis
-//    compileOnly("org.ow2.asm:asm-tree:9.4")
+    // https://central.sonatype.com/artifact/org.ow2.asm/asm/9.4
+//    implementation("org.ow2.asm:asm:9.4")
+    // https://central.sonatype.com/artifact/org.ow2.asm/asm-commons/9.4
+    implementation("org.ow2.asm:asm-commons:9.4") // 依赖 asm 以及 asm-tree
+    // https://central.sonatype.com/artifact/org.ow2.asm/asm-util/9.4
+//    implementation("org.ow2.asm:asm-util:9.4") // 依赖 asm 以及 asm-tree、asm-analysis
+    // https://central.sonatype.com/artifact/org.ow2.asm/asm-tree/9.4
+//    compileOnly("org.ow2.asm:asm-tree:9.4") // 依赖 asm
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation(gradleTestKit())
+//    testImplementation("com.android.tools.build:gradle:8.1.1")
+//    testImplementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
 }
